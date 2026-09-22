8628bf173e8b8e81b464aa3ffaa819f93718bcf7
Faisal Shehzad <shehf@farmingdale.edu>
2026-09-21 21:03:33 -0400

    Assignment 1 CSC229 course.java complete work

--- src/main/java/org/example/DriverClass.java
+++ src/main/java/org/example/DriverClass.java
@@ -1,10 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class DriverClass {
+
     public static void main(String[] args) {
 
-    }
+        Course course1 = new Course();
 
+        // Changing the course information using setters
+        course1.setId(1);
+        course1.setName("Data Structures and Algorithms");
+        course1.setCode("CSC229");
+
+        // Displaying the course information using getters
+        System.out.println("Course ID: " + course1.getId());
+        System.out.println("Course Name: " + course1.getName());
+        System.out.println("Course Code: " + course1.getCode());
+
+        // Creating another Course object using the overloaded constructor
+        Course course2 = new Course(2, "Java Programming", "CSC211");
+
+        System.out.println();
+        System.out.println("Second Course:");
+        System.out.println("Course ID: " + course2.getId());
+        System.out.println("Course Name: " + course2.getName());
+        System.out.println("Course Code: " + course2.getCode());
+    }
 }
